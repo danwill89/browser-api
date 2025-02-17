86d8f4d95a1d0873cf1f268ced2b6fab0747f79d
Daniel Williams <59125712+danwill89@users.noreply.github.com>
2025-02-17 16:49:35 +0000

    feat: Update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async headers() {
     return [
-      {
+      process.env.CODESPACES ? {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Forwarded-Host',
+            value: `${process.env.CODESPACE_NAME}-3000.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: `https://${process.env.CODESPACE_NAME}-3000.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
+          },
+        ],
+      } : {
         source: "/(.*)",
         headers: [
           {
@@ -12,17 +24,13 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "x-forwarded-host",
-            value: `${process.env.CODESPACE_NAME}-3000.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
-          },
-        ],
-      },
-    ];
+    ]
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"]
+    }
+  }
 };
 
 export default nextConfig;
