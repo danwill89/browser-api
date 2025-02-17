0add4ea3dcca684c712b3b4cd271d12e47efe631
Daniel Williams <59125712+danwill89@users.noreply.github.com>
2025-02-17 16:28:59 +0000

    feat: Updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "x-forwarded-host",
+            value: "localhost:3000",
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "x-forwarded-host",
+            value: `${process.env.CODESPACE_NAME}-3000.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
